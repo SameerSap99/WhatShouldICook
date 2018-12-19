aad5508a49795cdf9a3a99065720220bbd6ed5f0
Sameer99 <sameer.saptarshi>
2018-12-19 13:40:54 -0700

    Initialized

---
+++ What Should I Cook?/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What Should I Cook?
+//
+//  Created by Sameer Saptarshi on 12/19/18.
+//  Copyright © 2018 SameerSaptarshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
